71aa7a22e3a3a79c8e802f9024b0beee0837ba38
KKiumbe <97431990+KKiumbe@users.noreply.github.com>
2024-10-10 16:25:11 +0300

    mpesa

--- routes/mpesa/mpesaRoute.js
+++ routes/mpesa/mpesaRoute.js
@@ -1,13 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const axios = require('axios');
-// const { generateAccessToken } = require('./mpesaMiddleware'); // Adjust the path as needed
 
-// // Your M-Pesa credentials
-// const lipaNaMpesaOnlineShortcode = process.env.MPESA_SHORTCODE; // Lipa Na M-Pesa shortcode
-// const baseUrl = 'https://sandbox.safaricom.co.ke'; // Change to live URL for production
+const { lipaNaMpesa } = require('../../controller/mpesa/payment.js');
 
-// // Route to receive M-Pesa payment notifications
 router.post('/callback', async (req, res) => {
     const paymentData = req.body; // M-Pesa sends the payment details in the body
 
@@ -18,28 +13,7 @@ router.post('/callback', async (req, res) => {
     res.status(200).send('Notification received'); // Respond with 200 OK to M-Pesa
 });
 
-// Route to check transaction status with the token middleware
-// router.get('/transaction-status/:transactionId', generateAccessToken, async (req, res) => {
-//     const transactionId = req.params.transactionId;
 
-//     try {
-//         const response = await axios.get(`${baseUrl}/mpesa/transactionstatus/v1/query`, {
-//             headers: {
-//                 Authorization: `Bearer ${req.accessToken}`, // Use the token from the request object
-//             },
-//             params: {
-//                 Shortcode: lipaNaMpesaOnlineShortcode,
-//                 TransactionID: transactionId,
-//                 // Include other necessary parameters required by the API
-//             },
-//         });
-
-        // Return the response from M-Pesa
-//         res.status(200).json(response.data);
-//     } catch (error) {
-//         console.error('Error fetching transaction status:', error);
-//         res.status(500).json({ error: 'Error fetching transaction status' });
-//     }
-// });
+router.post('/lipa', lipaNaMpesa); // Use the controller function
 
 module.exports = router;
