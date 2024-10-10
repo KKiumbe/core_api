771a47a60b15304e1c99c62f8a0dd0b548b57824
KKiumbe <97431990+KKiumbe@users.noreply.github.com>
2024-10-10 17:01:46 +0300

    mpesa

--- routes/mpesa/mpesaRoute.js
+++ routes/mpesa/mpesaRoute.js
@@ -6,8 +6,13 @@ const { lipaNaMpesa } = require('../../controller/mpesa/payment.js');
 router.post('/callback', async (req, res) => {
     const paymentData = req.body; // M-Pesa sends the payment details in the body
 
+    if(!paymentData){
+        res.json('ok')
+    }
+
     // Handle the payment notification (e.g., save to database, send confirmation, etc.)
-    console.log('Payment Notification Received:', paymentData);
+    console.log('Payment Notification Received:', paymentData.body);
+    console.log(paymentData.Body.CallbackMetadata);
     
     // Send a response back to M-Pesa
     res.status(200).send('Notification received'); // Respond with 200 OK to M-Pesa
