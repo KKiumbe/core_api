e0510b6905fca77061b973078e8af631434b6881
KKiumbe <97431990+KKiumbe@users.noreply.github.com>
2024-10-15 01:29:17 +0300

    latest1

--- routes/mpesa/mpesaRoute.js
+++ routes/mpesa/mpesaRoute.js
@@ -1,4 +1,8 @@
 const express = require('express');
+
+const { PrismaClient } = require('@prisma/client');
+
+const prisma = new PrismaClient();
 const router = express.Router();
 
 const { lipaNaMpesa } = require('../../controller/mpesa/payment.js');
