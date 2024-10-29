4c1c57686ec70a89f55c4f6d6a449bc140769dcd
KKiumbe <97431990+KKiumbe@users.noreply.github.com>
2024-10-29 22:33:09 +0300

    create customer details added

--- routes/receipt/receiptingRoute.js
+++ routes/receipt/receiptingRoute.js
@@ -1,11 +1,10 @@
 const express = require('express');
-const { manualReceipt } = require('../../controller/receipting/manualReceipting.js');
 const { getReceipts, getReceiptById } = require('../../controller/receipting/getReceipt.js');
 const { manualCashPayment } = require('../../controller/receipting/manualCashPayment.js');
 
 const router = express.Router();
 
-router.post('/manual-receipt', manualReceipt);
+router.post('/manual-receipt', manualCashPayment);
 router.post('/manual-cash-payment', manualCashPayment);
 
 router.get('/receipts',getReceipts );
