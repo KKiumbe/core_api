330aaa8b86838e5cd023beaefaa8c4626a5334f6
KKiumbe <97431990+KKiumbe@users.noreply.github.com>
2024-10-30 02:04:46 +0300

    sending payment confirmation sms

--- routes/sms/sms.js
+++ routes/sms/sms.js
@@ -9,6 +9,7 @@ const PARTNER_ID = process.env.PARTNER_ID;
 const SHORTCODE = process.env.SHORTCODE;
 //const BULK_SMS_ENDPOINT = process.env.BULK_SMS_ENDPOINT;
 const SMS_ENDPOINT = process.env.SMS_ENDPOINT;
+console.log(SMS_ENDPOINT);
 
 
 
@@ -19,13 +20,23 @@ const sendSMS = async (mobile, message) => {
 
     console.log(`sanitised number is ${mobile}`);
     try {
-       const payload = {
-            partnerID: PARTNER_ID,
-            apikey: SMS_API_KEY,
-            mobile: mobile,
-            message: message,
-            shortcode: SHORTCODE,
-        }
+    //    const payload = {
+    //         partnerID: PARTNER_ID,
+    //         apikey: SMS_API_KEY,
+    //         mobile,
+    //         message,
+    //         shortcode: SHORTCODE,
+    //     }
+
+
+    const payload = {
+        partnerID: "4680",
+        apikey: "146d35f516edcdaf3cc86bb388f8afde",
+        mobile: "254702550190", // Flatten the mobile field
+        message: "Dear Kevin, payment received successfully. Your closing balance is an overpayment of KES 1. Thank you for your payment!", // Separate the message field
+        shortcode: "SIKIKA_LTD"
+    };
+    
 
         console.log(`this is payload ${payload}`);
         const response = await axios.post(SMS_ENDPOINT,payload
