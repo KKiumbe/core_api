a4e360c1068e8c026cd2c9a71564f87f1ca3daed
KKiumbe <97431990+KKiumbe@users.noreply.github.com>
2024-10-30 02:52:09 +0300

    sending payment confirmation sms

--- routes/sms/sms.js
+++ routes/sms/sms.js
@@ -1,48 +1,26 @@
-const axios = require('axios'); // Ensure axios is imported
-
-
-
+const axios = require('axios');
 
 // Load environment variables
 const SMS_API_KEY = process.env.SMS_API_KEY;
 const PARTNER_ID = process.env.PARTNER_ID;
 const SHORTCODE = process.env.SHORTCODE;
-//const BULK_SMS_ENDPOINT = process.env.BULK_SMS_ENDPOINT;
 const SMS_ENDPOINT = process.env.SMS_ENDPOINT;
-console.log(SMS_ENDPOINT);
-
-
-
-
-
 
 const sendSMS = async (sanitisedNumber, message) => {
-
-    console.log(`sanitised number is ${sanitisedNumber}`);
+    console.log(`Sanitised number is ${sanitisedNumber}`);
     try {
-       const payload = {
+        const payload = {
             partnerID: PARTNER_ID,
             apikey: SMS_API_KEY,
-            mobile:sanitisedNumber,
+            mobile: sanitisedNumber,
             message,
             shortcode: SHORTCODE,
-        }
+        };
 
+        console.log(`This is payload: ${JSON.stringify(payload)}`);
 
-    // const payload = {
-    //     partnerID: "4680",
-    //     apikey: "146d35f516edcdaf3cc86bb388f8afde",
-    //     mobile: "254702550190", // Flatten the mobile field
-    //     message: "Dear Kevin, payment received successfully. Your closing balance is an overpayment of KES 1. Thank you for your payment!", // Separate the message field
-    //     shortcode: "SIKIKA_LTD"
-    // };
-    
-
-    console.log(`this is payload ${JSON.stringify(payload)}`);
-
-        const response = await axios.post(SMS_ENDPOINT,payload
-        );
-        console.log(`SMS sent to ${mobile}: ${message}`);
+        const response = await axios.post(SMS_ENDPOINT, payload);
+        console.log(`SMS sent to ${sanitisedNumber}: ${message}`);
         return response.data; // Return the response for further processing if needed
     } catch (error) {
         console.error('Error sending SMS:', error);
@@ -52,4 +30,4 @@ const sendSMS = async (sanitisedNumber, message) => {
 
 module.exports = {
     sendSMS
-}
+};
