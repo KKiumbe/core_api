c7267a0486645e1d680c9f8f5b76c981e4bf5a26
KKiumbe <97431990+KKiumbe@users.noreply.github.com>
2024-11-04 20:27:51 +0300

    debt report

--- routes/reportRoutes/reportRoute.js
+++ routes/reportRoutes/reportRoute.js
@@ -1,10 +1,11 @@
 // routes/reportRoutes.js
 const express = require('express');
 const { getCustomersWithDebtReport } = require('../../controller/reports/debtReport.js');
+const { getAllActiveCustomersReport } = require('../../controller/reports/allCustomers.js');
 const router = express.Router();
 
 // Define the route for the debt report
 router.get('/reports/customers-debt', getCustomersWithDebtReport);
-router.get('/reports/customers', getCustomersWithDebtReport);
+router.get('/reports/customers', getAllActiveCustomersReport);
 
 module.exports = router;
